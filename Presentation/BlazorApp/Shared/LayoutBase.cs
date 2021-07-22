4742c0537546f5fad9278fbc245c21e751788332
Morri <kirill.lifarenko@gmail.com>
2021-07-22 20:42:11 +0300

    ++

---
+++ Presentation/BlazorApp/Shared/LayoutBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public abstract class LayoutBase : ComponentBase
+    {
+        protected LayoutBase()
+        {
+
+        }
+
+        public void RefreshState()
+        {
+            StateHasChanged();
+        }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+    }
+}
